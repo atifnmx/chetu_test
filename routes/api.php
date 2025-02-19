bc011b8bec42542996335cd297c32f6548df7844
atif <atif.ali@nmxglobalsoftware.com>
2025-02-19 17:05:19 +0500

    'c'

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/test', function (Request $request) {
-    return ['name' => 'working.'];
+    return ['name' => 'working...'];
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
